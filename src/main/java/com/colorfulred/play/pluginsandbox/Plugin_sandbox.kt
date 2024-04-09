e411373a3b1876f0e7e06609379c5dbe5a1c6a73
tkuramot <mabdman512@gmail.com>
2024-04-10 03:32:15 +0900

    Initial commit

---
+++ src/main/java/com/colorfulred/play/pluginsandbox/Plugin_sandbox.kt
@@ -0,0 +1,13 @@
+package com.colorfulred.play.pluginsandbox
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Plugin_sandbox : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
